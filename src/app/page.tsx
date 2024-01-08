81c3c8ad6265471c3411527a96f1c4d2be0985ee
Manav <mjfx102@gmail.com>
2024-01-09 03:30:30 +0530

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,107 @@
+"use client"
 import Image from 'next/image'
-
+import React, { use, useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import { JsonRpcApiProvider } from 'ethers';
+import { JsonRpcProvider ,  } from 'ethers';
+import Ethereum from '../assets/Eth.png'
+import Profile from '../assets/Nft.png'
+import Web3Modal from "web3modal";
 export default function Home() {
+  const [account, setAccount] = useState("");
+  const [balance , setBalance] = useState("");
+  const [connect , setConnect] = useState(false);
+  const failMessage = "Failed to Connect with Your Metamask account. Please Check and retry again"
+  const provider = new JsonRpcProvider(`https://mainnet.infura.io/v3/b27ad50709544a07a600c08e4a7ffe95`);
+  const checkIfWalletConnected = async () =>{
+    if(!window.ethereum) return;
+    const accounts = await window.ethereum.request({method : "eth_accounts"});
+    console.log(accounts);
+    if(accounts.length){
+      setAccount(accounts[0]); 
+    }
+    else{
+      // return failMessage;
+      console.log("Fail");
+    }
+    const address = "0xF02c1c8e6114b1Dbe8937a39260b5b0a374432bB"
+    const balance = await provider.getBalance(address);
+    const showBalance = `${ethers.formatEther(balance)}-ETH`
+    console.log(showBalance);
+    setBalance(showBalance);
+    
+  }
+  const connectWallet = async ()=>{
+   if(!window.ethereum) return console.log(failMessage);
+   const accounts = await window.ethereum.request({method: "eth_requestAccounts"});
+   setAccount(accounts[0]);
+   window.location.reload();
+  }
+  useEffect(()=>{
+    checkIfWalletConnected();
+  })
+  useEffect(()=>{
+    async function accountChanged(){
+      window.ethereum.on("accountsChanged" , async function(){
+        const accounts = await window.ethereum.request(
+          {
+            method: "eth_accounts"
+          }
+        );
+        if(accounts.length){
+          setAccount(accounts[0]);
+        }else{
+          window.location.reload();
+        }
+      })
+    }
+    accountChanged()
+  },[])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  <div className="card">
+    {!account ? "" : <span className='pro'>PRO</span>}
+    <Image src ={Profile} alt={"Profile Image Alt Text"} width={90} height={90}/>
+    <h3> Check Ether</h3>
+     {!account ? (
+      <div>
+        <div className="message">
+          <p>{failMessage}</p>
         </div>
+        <Image src ={Ethereum} alt='Ethereum Image Alt Text' width={90} height={90}/>
+        <p> Welcome to the Wallet Dashboard</p>
+        </div>
+     ):(
+      <div>
+        <h5>Verified Account <span className='tick'>&#10004;</span></h5>
+        <p>Ether Account and balance Checker <br/> Find account details</p>
+        <div className="buttons">
+          <button className='primary ghost' onClick={()=>{
+
+          }}>
+            Ether Account Details
+          </button>
+        </div>
+        </div>
+     )
+    
+    }
+    {!account && !connect ? (
+      <div className="buttons">
+        <button className='primary' onClick={()=>connectWallet()}>Connect Wallet</button>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    ):
+    (
+      <div className="skills">
+        <h6>Ethers</h6>
+        <ul>
+          <li>Account</li>
+          <li>{account}</li>
+          <li>Balance</li>
+          <li>{balance}</li>
+        </ul>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    )
+    }
+  </div>
+  );
+};
